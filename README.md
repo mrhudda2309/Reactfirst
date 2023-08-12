afcd2c2123c4fc596879b6b4a3ee05ff0010fd9e
mrhudda2309 <139607477+mrhudda2309@users.noreply.github.com>
2023-08-12 15:51:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reactfirst
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-udfyv1)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Create from './components/create';
+
+function App() {
+  return (
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
+      <div>
+        <Create />
+      </div>
+    </div>
+  );
+}
+
+export default App;
